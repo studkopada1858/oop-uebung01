0d3769dbf1755c468d20925b9c0b35d96973946d
studkopada1858 <damian.kopal@gmx.de>
2019-03-26 15:15:14 +0100

    jo

---
+++ src/main/java/ueb01/CharElement.java
@@ -0,0 +1,17 @@
+package ueb01;
+
+public class CharElement {
+
+    char c;
+        CharElement next;
+
+        public CharElement (char c, CharElement next){
+            this.c=c;
+            this.next=next;
+        }
+        public char value(){
+            return c;
+
+        }
+    }
+
--- src/main/java/ueb01/CharStackImpl.java
+++ src/main/java/ueb01/CharStackImpl.java
@@ -1,4 +1,36 @@
 package ueb01;
 
-class CharStackImpl implements CharStack {
+import java.util.NoSuchElementException;
+
+class CharStackImpl implements CharStack{
+
+    CharElement top;
+
+
+    @Override
+    public void push(char c){
+        top=new CharElement(c,top);
+
+    }
+
+    @Override
+    public char pop() {
+        if(top==null){
+            throw new NoSuchElementException();
+        }
+        char rem=top.value();
+        top=top.next;
+        return rem;
+    }
+
+    @Override
+    public int size() {
+        int n = 0;
+        CharElement it = top;
+        while (it != null) {
+            n++;
+            it = it.next;
+        }
+        return n;
+    }
 }
